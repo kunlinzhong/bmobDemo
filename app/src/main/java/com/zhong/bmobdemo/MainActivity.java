bed0a642567bb74889cf168deb12b0862059f08b
zhongkunlin@comodin.cn <zhongkunlin@comodin.cn>
2016-11-22 18:39:29 +0800

    zhongkunlin

--- app/src/main/java/com/zhong/bmobdemo/MainActivity.java
+++ app/src/main/java/com/zhong/bmobdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zhong.bmobdemo;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.zhong.tool.Person;
 
 import org.json.JSONArray;
@@ -27,6 +32,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Context con;
     private Button save_btn,query_btn,bmobquery_btn,update_btn,delete_btn;
     private int number = 0;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +60,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bmobquery_btn.setOnClickListener(this);
         update_btn.setOnClickListener(this);
         delete_btn.setOnClickListener(this);
+        setUp();
     }
+
+    private void setUp() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(con);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Battery_Saving，低功耗模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(1000);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否强制刷新WIFI，默认为true，强制刷新。
+        mLocationOption.setWifiActiveScan(false);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+//启动定位
+        mLocationClient.startLocation();
+    }
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @SuppressLint("LongLogTag")
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+
+            if (aMapLocation != null) {
+                if (aMapLocation.getErrorCode() == 0) {
+                    //可在其中解析amapLocation获取相应内容。
+                    Log.e("getErrorCode", "ErrorCode is 0");
+                    Log.e("getLatitude  getLongitude",aMapLocation.getLatitude()+"---"+aMapLocation.getLongitude());
+                    Log.e(" getAddress", aMapLocation.getAddress());
+                }else {
+                    //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                    Log.e("AmapError","location Error, ErrCode:"
+                            + aMapLocation.getErrorCode() + ", errInfo:"
+                            + aMapLocation.getErrorInfo());
+                }
+            }else{
+                Log.e("aMapLocation", "aMapLocation is null");
+            }
+
+        }
+    };
 
     @Override
     public void onClick(View v) {
